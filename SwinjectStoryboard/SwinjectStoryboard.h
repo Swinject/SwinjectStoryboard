7d62a8941695fdce17a630e316eec96ae3288574
Yoichi Tagaya <yoichi@el-eleven.com>
2016-05-05 14:00:46 +0900

    The initial commit.

---
+++ SwinjectStoryboard/SwinjectStoryboard.h
@@ -0,0 +1,19 @@
+//
+//  SwinjectStoryboard.h
+//  SwinjectStoryboard
+//
+//  Created by Yoichi Tagaya on 5/5/16.
+//  Copyright © 2016 Swinject Contributors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwinjectStoryboard.
+FOUNDATION_EXPORT double SwinjectStoryboardVersionNumber;
+
+//! Project version string for SwinjectStoryboard.
+FOUNDATION_EXPORT const unsigned char SwinjectStoryboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwinjectStoryboard/PublicHeader.h>
+
+
