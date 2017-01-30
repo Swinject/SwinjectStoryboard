cc92d3120b211a03b34436dedab1ec5f0a2ab44f
Roman <sfm.kmr@gmail.com>
2017-01-30 11:23:30 +0200

    Updated swift package manager dependencies.

--- Package.swift
+++ Package.swift
@@ -2,4 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "SwinjectStoryboard"
+        dependencies: [
+        .Package(url: "https://github.com/Swinject/Swinject.git", majorVersion: 2, minor: 0)
+    ]
+
 )
