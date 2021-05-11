1e84e57379b11bd4c5abfec89299bb64bd132b6c
Dmytro Pavlenko <basket.pavlenko@gmail.com>
2021-05-11 09:28:45 +0300

    feature(xcode12.5): update for a new xcode version and swift 5

--- Sources/SwinjectStoryboardOption.swift
+++ Sources/SwinjectStoryboardOption.swift
@@ -10,6 +10,11 @@ import Swinject
 
 #if os(iOS) || os(OSX) || os(tvOS)
 internal struct SwinjectStoryboardOption: ServiceKeyOption {
+    
+    func hash(into: inout Hasher) {
+        
+    }
+    
     internal let controllerType: String
     
     internal init(controllerType: Container.Controller.Type) {
